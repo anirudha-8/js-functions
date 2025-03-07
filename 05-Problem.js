8e1e852833bf36a276c270e8c201c149621d9330
Anirudha Bele <beleanirudha8@gmail.com>
2025-03-07 10:31:07 +0530

    perf(05-Problem.js): optimize code by handling edge cases

--- 05-Problem.js
+++ 05-Problem.js
@@ -1,7 +1,12 @@
 // 05-Problem statement - Write a function to find maximum of two numbers.
 
 function maximumOfTwoNumbers(number1, number2) {
-	return number1 >= number2 ? number1 : number2;
+	if (typeof number1 !== "number" || typeof number2 !== "number")
+		return "Invalid input! Provide numbers input only!";
+	if (number1 === number2) return "Both numbers are equal!";
+
+	// return number1 >= number2 ? number1 : number2;
+	return Math.max(number1, number2);
 }
 
 const number1 = 8;
@@ -10,4 +15,8 @@ const number2 = 88;
 
 const result = maximumOfTwoNumbers(number1, number2);
 
-console.log(`Maximum between ${number1} and ${number2} is: ${result}`);
+console.log(
+	typeof result === "number"
+		? `Maximum between ${number1} and ${number2} is: ${result}`
+		: result
+);

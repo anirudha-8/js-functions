e3979ff740af127cebe9692fdf3200c41935492e
Anirudha Bele <beleanirudha8@gmail.com>
2025-03-08 09:51:52 +0530

    feat(09-Problem.js): add solution for 09-Problem

---
+++ 09-Problem.js
@@ -0,0 +1,16 @@
+// 09-Problem statement - Write a function that converts a string to kebab-case (e.g - "Hello World" -> "hello-world").
+
+function toKebabCaseConversion(str) {
+	if (typeof str !== "string" || !str.trim().length)
+		return "Invalid input! Provide non-empty string in input!";
+
+	return str.trim().toLowerCase().replace(/\s+/g, "-");
+}
+
+const name = "Anirudha  Bele";
+
+const kebabCaseResult = toKebabCaseConversion(name);
+
+console.log(
+	`kebab-case version of the string "${name}" is: "${kebabCaseResult}"`
+);

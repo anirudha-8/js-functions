dd62549d86920946040871053ff0cca235996453
Anirudha Bele <beleanirudha8@gmail.com>
2025-03-08 10:06:28 +0530

    feat(10-Problem.js): add solution for 10-Problem

---
+++ 10-Problem.js
@@ -0,0 +1,14 @@
+// 10-Problem statement - Create a function that logs "Hello, World!" every time it is called.
+
+function logHelloWorld() {
+	console.log("Hello, World!");
+}
+
+logHelloWorld();
+logHelloWorld();
+logHelloWorld();
+logHelloWorld();
+logHelloWorld();
+logHelloWorld();
+logHelloWorld();
+logHelloWorld();
